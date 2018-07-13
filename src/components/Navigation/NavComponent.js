60e267d83d163df54ed5ad83923fb3e927f14981
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-07-12 17:58:42 -0700

    started conditional rendering for nav, switching branches

--- src/components/Navigation/NavComponent.js
+++ src/components/Navigation/NavComponent.js
@@ -14,6 +14,45 @@ export default class NavComponent extends Component {
   };
 
   render() {
+    if (localStorage.token) {
+      return this._renderLoggedIn();
+    }
+    return this._renderLoggedOut();
+  }
+  _renderLoggedin() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu inverted size="small">
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="mentees"
+          active={activeItem === "mentees"}
+          onClick={this.handleMenteesClick}
+        />
+        <Menu.Item
+          name="mentors"
+          active={activeItem === "mentors"}
+          onClick={this.handleMentorsClick}
+        />
+
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Button primary>Sign Up</Button>
+          </Menu.Item>
+          <Menu.Item>
+            <Button primary>Login</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+
+  _renderLoggedOut() {
     const { activeItem } = this.state;
 
     return (
