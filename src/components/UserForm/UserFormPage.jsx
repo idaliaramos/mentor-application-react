7497e283549ebd6ad3a0a16e8cabf78c4f35ef39
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-07-18 12:41:42 -0700

    added proptype validation to user form function

--- src/components/UserForm/UserFormPage.jsx
+++ src/components/UserForm/UserFormPage.jsx
@@ -1,24 +1,29 @@
-import React, { Component } from "react";
-import NavComponent from "../NavComponent";
-import UserFormComponent from "./UserFormComponent";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import NavComponent from '../NavComponent';
+import UserFormComponent from './UserFormComponent';
 
 export default class UserFormPage extends Component {
   constructor() {
     super();
-    this.state = {
-      userInfo: {}
-    };
+    this.state = {};
   }
 
   render() {
+    const { onSubmitUserInfo } = this.props;
     return (
       <div className="UserPage">
         <NavComponent />
-        <h1 className="center">Tell us about yourself</h1>
+        <h1 className="center">
+Tell us about yourself
+        </h1>
         <div className="form-component">
-          <UserFormComponent onSubmitUserInfo={this.props.onSubmitUserInfo} />
+          <UserFormComponent onSubmitUserInfo={onSubmitUserInfo} />
         </div>
       </div>
     );
   }
 }
+UserFormPage.propTypes = {
+  onSubmitUserInfo: PropTypes.func.isRequired,
+};
