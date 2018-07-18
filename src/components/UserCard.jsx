cb3fb0c72e5b7c860b9dd6f83a5e871c78656217
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-07-18 11:47:10 -0700

    refactord userCard to have Proptype validation

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,33 +1,47 @@
-import React from "react";
-import { Card, Icon, Image, Label, Button } from "semantic-ui-react";
+import React from 'react';
+import {
+  Card, Icon, Image, Label,
+} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
 const style = {
-  margin: 10
+  margin: 10,
 };
-const UserCard = props => {
-  return (
-    <div key={props.id}>
-      <Card style={style} key={props.id}>
-        <Card.Content key={props.id}>
-          <Image floated="right" size="tiny" src={props.image} />
-          <Card.Header>{props.name}</Card.Header>
-          <Card.Meta>
-            <Label.Group color="blue">
-              {props.skills.map(skill => <Label as="a">{skill}</Label>)}
-            </Label.Group>
-          </Card.Meta>
-          <Card.Description>{props.message}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <a>
-            <Icon name="user" />
-            {props.contact}
-          </a>
-        </Card.Content>
-      </Card>
-    </div>
-  );
-  // alternate layout{
-  /* <Card.Group>
+const UserCard = ({
+  id, image, name, skills, contact, message,
+}) => (
+  <div key={id}>
+    <Card style={style} key={id}>
+      <Card.Content key={id}>
+        <Image floated="right" size="tiny" src={image} />
+        <Card.Header>
+          {name}
+        </Card.Header>
+        <Card.Meta>
+          <Label.Group color="blue">
+            {console.log(skills, 'skills')}
+            {skills.map(skill => (
+              <Label as="a">
+                {skill}
+              </Label>
+            ))}
+          </Label.Group>
+        </Card.Meta>
+        <Card.Description>
+          {message}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a href={contact}>
+          <Icon name="user" />
+          {contact}
+        </a>
+      </Card.Content>
+    </Card>
+  </div>
+);
+// alternate layout{
+/* <Card.Group>
   <Card style={style} color="blue">
     <Image src={props.image} size="s" />
     <Card.Content>
@@ -49,5 +63,15 @@ const UserCard = props => {
 </Card.Group>
 
 {/* Layout 2 */
+UserCard.defaultProps = {
+  image: 'hello',
+};
+UserCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  contact: PropTypes.string.isRequired,
+  skills: PropTypes.arrayOf(PropTypes.string).isRequired,
+  image: PropTypes.string,
+  message: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
 export default UserCard;
