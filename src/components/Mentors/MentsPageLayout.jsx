bde9f6d20ff89858f2ab7d80cc2f8e9f8ef5ef0e
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-07-18 12:35:08 -0700

    fixed page layout standardization

--- src/components/Mentors/MentsPageLayout.jsx
+++ src/components/Mentors/MentsPageLayout.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React from 'react';
 
 export default function MentsPageLayout(prop) {
   const style = {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    marginLeft: 40
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginLeft: 40,
   };
-  let cards = prop.children.map(child => child);
+  const cards = prop.children.map(child => child);
   return (
     <div>
-      <div style={style}>{cards}</div>
+      <div style={style}>
+        {cards}
+      </div>
     </div>
   );
 }
