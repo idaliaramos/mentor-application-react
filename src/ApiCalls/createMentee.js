8eaff5b82a55215d76e8b96a3c19d793244738bd
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-07-25 17:35:46 -0700

    added comments

--- src/ApiCalls/createMentee.js
+++ src/ApiCalls/createMentee.js
@@ -1,14 +1,13 @@
-export default function(userInfo) {
+//api request to post a new mentee
+export default function (userInfo) {
   fetch(`${process.env.REACT_APP_DATABASE_URL}/mentees`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.REACT_APP_KEY}`
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.REACT_APP_KEY}`,
     },
     body: JSON.stringify({
-      fields: userInfo
-    })
-  }).then(response => {
-    return response.json();
-  });
+      fields: userInfo,
+    }),
+  }).then(response => response.json());
 }
